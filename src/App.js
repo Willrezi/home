fc6868ba7d54adaaa001d665126816c17b1ca94c
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-04-02 15:49:38 +0200

    react hook useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [userQuery, setUserQuery] = useState("");
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value);
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      searchQuery();
+    }
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Will</h1>
+      <div>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
